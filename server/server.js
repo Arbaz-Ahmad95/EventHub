56c933394159fdd094e7e888f3cacb99afe2194c
Arbaz-Ahmad95 <arbazahmad363@gmail.com>
2025-12-18 15:34:06 +0530

    backend url

--- server/server.js
+++ server/server.js
@@ -8,15 +8,24 @@ const eventRoutes = require("./routes/eventRoutes");
 
 const app = express();
 
-// middlewares
-app.use(cors());
+// ✅ CORS (Vercel frontend)
+app.use(cors({
+  origin: "https://event-hub-inky.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-
 
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 // mongo connection
 mongoose
@@ -24,13 +33,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Mongo error:", err.message));
 
+// ✅ PORT fix (VERY IMPORTANT for Render)
+const PORT = process.env.PORT || 5000;
 
-  app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-
-// server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
