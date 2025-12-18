1e66db6e07a580ebdee67ab727462e8bdc848d63
Arbaz-Ahmad95 <arbazahmad363@gmail.com>
2025-12-18 15:10:00 +0530

    backend updated routes

--- server/server.js
+++ server/server.js
@@ -24,6 +24,12 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Mongo error:", err.message));
 
+
+  app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
 // server start
 app.listen(5000, () => {
   console.log("Server running on port 5000");
